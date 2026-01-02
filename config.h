4ecccb6087a41247d7689a0bb5edc71cd1e5509d
Cougar <cougar@random.ee>
2026-01-02 20:39:21 +0200

    Add pcmanfm hotkey

--- config.h
+++ config.h
@@ -60,6 +60,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "uxterm", NULL };
 static const char *lockcmd[]  = { "xlock", NULL };
+static const char *pcmanfm[]  = { "pcmanfm", NULL };
 
 #include "selfrestart.c"
 
@@ -68,6 +69,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ SUPKEY,                       XK_r,      spawn,          {.v = termcmd } },
 	{ SUPKEY,                       XK_l,      spawn,          {.v = lockcmd } },
+	{ SUPKEY,                       XK_e,      spawn,          {.v = pcmanfm } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
